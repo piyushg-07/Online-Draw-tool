5d3e3dc71a398e0f843bc700fc795f2c42bb6deb
Piyush Gupta <guptapiyush315@gmail.com>
2024-05-28 22:20:21 +0530

    frontend UI

--- sketchbook-draw-tool/src/components/Menu/index.js
+++ sketchbook-draw-tool/src/components/Menu/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import cx from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 
-const index = () => {
+import styles from './index.module.css'
+
+import { MENU_ITEMS } from '@/constants'
+const Menu = () => {
   return (
-    <div>index</div>
+    <div className={styles.menuContainer}>
+    <div className={cx(styles.iconWrapper, )} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+        <FontAwesomeIcon icon={faPencil} className={styles.icon} />
+    </div>
+    <div className={cx(styles.iconWrapper, )} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+        <FontAwesomeIcon icon={faEraser} className={styles.icon} />
+    </div>
+    <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.UNDO)}>
+        <FontAwesomeIcon icon={faRotateLeft} className={styles.icon}/>
+    </div>
+    <div className={styles.iconWrapper} onClick={() => handleActioItemClick(MENU_ITEMS.REDO)}>
+        <FontAwesomeIcon icon={faRotateRight} className={styles.icon}/>
+    </div>
+    <div className={styles.iconWrapper}  onClick={() => handleActioItemClick(MENU_ITEMS.DOWNLOAD)}>
+        <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon}/>
+    </div>
+</div>
   )
 }
 
-export default index
+export default Menu
--- sketchbook-draw-tool/src/components/Toolbox/index.js
+++ sketchbook-draw-tool/src/components/Toolbox/index.js
@@ -1,9 +1,34 @@
- import React from 'react'
- 
- const index = () => {
-   return (
-     <div>index</div>
-   )
- }
- 
- export default index
+import cx from 'classnames';
+
+import styles from './index.module.css'
+
+import { COLORS, MENU_ITEMS } from '@/constants'
+const Toolbox = () => {
+  return (
+    <div className={styles.toolboxContainer}>
+      {/* {showStrokeToolOption &&  */}
+      <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Stroke Color</h4>
+        <div className={styles.itemContainer}>
+          <div className={cx(styles.colorBox,)} style={{ backgroundColor: COLORS.BLACK }} onClick={() => updateColor(COLORS.BLACK)} />
+          <div className={cx(styles.colorBox, )} style={{ backgroundColor: COLORS.RED }} onClick={() => updateColor(COLORS.RED)} />
+          <div className={cx(styles.colorBox, )} style={{ backgroundColor: COLORS.GREEN }} onClick={() => updateColor(COLORS.GREEN)} />
+          <div className={cx(styles.colorBox, )} style={{ backgroundColor: COLORS.BLUE }} onClick={() => updateColor(COLORS.BLUE)} />
+          <div className={cx(styles.colorBox,)} style={{ backgroundColor: COLORS.ORANGE }} onClick={() => updateColor(COLORS.ORANGE)} />
+          <div className={cx(styles.colorBox, )} style={{ backgroundColor: COLORS.YELLOW }} onClick={() => updateColor(COLORS.YELLOW)} />
+        </div>
+      </div>
+      {/* } */}
+      {/* {showBrushToolOption && */}
+       <div className={styles.toolItem}>
+        <h4 className={styles.toolText}>Brush Size</h4>
+        <div className={styles.itemContainer}>
+          <input type="range" min={1} max={10} step={1}  />
+        </div>
+      </div>
+      {/* // } */}
+    </div>
+  )
+}
+
+export default Toolbox
